e622e7e49dd3327f29470be88d9b8a9e39686f8f
Alaeron <jmcstarwars@gmail.com>
2026-04-24 23:39:14 +0000

    Fix build issues

--- src/lib/server/db/client.ts
+++ src/lib/server/db/client.ts
@@ -1,12 +1,17 @@
+import { building } from '$app/environment';
 import { sql } from "drizzle-orm";
 import { drizzle } from 'drizzle-orm/libsql';
 
-const db = drizzle({
-    connection: {
-        url: `file://${process.env.DB_URL}`
-    }
-});
+let db = undefined as unknown as db;
 
-db.run(sql`PRAGMA foreign_keys = ON`);
+if (!building) {
+    db = drizzle({
+        connection: {
+            url: `file://${process.env.DB_URL}`
+        }
+    });
+
+    db.run(sql`PRAGMA foreign_keys = ON`);
+}
 
 export { db };
